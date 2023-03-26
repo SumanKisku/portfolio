81c04a031ce645c614f0174011206d2b0897ec7d
Suman Kisku <sumankisku1@gmail.com>
2023-03-27 01:56:28 +0530

    change something

--- src/pages/components/About/index.js
+++ src/pages/components/About/index.js
@@ -2,20 +2,20 @@ import Image from 'next/image'
 
 const About = () => {
   return (
-    <div className="m-10 border-green-500">
+    <div id="about" className="md:grid md:grid-cols-2 mt-20 mx-5 xl:mx-52">
         <div className="mx-auto">
 
         <Image
-        className="rounded-lg mt-5 md:w-60 md:h-44"
+        className="rounded-lg mt-5 w-full md:w-60 md:h-44"
         src="/images/laptop.jpg"
         height={200}
         width={200}
         alt="Laptop"
         />
-        <div className="flex justify-center items-center h-24 w-24 md:h-36 md:w-36 relative -top-16 left-36 bg-white rounded-full">
+        <div className="flex justify-center items-center h-24 w-24 md:h-28 md:w-28 relative -top-12 left-1/2 -translate-x-1/2 md:left-60 md:-top-20 bg-white rounded-full">
             <Image 
             src="/images/circle.svg" 
-            className="h-24 w-24 md:h-36 md:w-36 absolute spin-text duration-5000"
+            className="h-full w-full  absolute spin-text duration-5000"
             alt="front end developer"
             width={96}
             height={96}
@@ -23,6 +23,13 @@ const About = () => {
 
         🧑🏿‍💻
         </div>
+        </div>
+        <div className="">
+          <h2 className="font-bold text-blue-500 text-xl">About Me</h2>
+          <h3 className="font-black text-gray-900 text-2xl">A dedicated Front End Developer based in West Bengal, India</h3>
+          <p className="text-gray-500">
+          As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.
+          </p>
         </div>
     </div>
   )
--- src/pages/components/Navbar/index.js
+++ src/pages/components/Navbar/index.js
@@ -1,15 +1,27 @@
-// TODO - Make navbar resposive
+
+import {useState} from "react"
 
 function Navbar() {
-	return(
+	const [visibility, setVisibility] = useState(false);
+	function handleClick() {
+		console.log("1", visibility);
+		setVisibility(!visibility);
+		console.log("2", visibility);
+	}
+	return (
 		<nav className="flex flex-col items-center md:flex-row justify-between mx-8 mt-5 font-bold">
-			<a href="*">Suman.portflio</a>
-			<ul className="flex gap-3">
-				<li><a href="#home">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#projects">Projects</a></li>
-				<li><a href="#contact">Contact</a></li>
-			</ul>	
+			<div onClick={handleClick} className="absolute right-10 cursor-pointer z-10 md:hidden">
+				<div className={`h-[4px] w-8 ease-out duration-200 origin-bottom-right ${visibility ? `-rotate-45 bg-white` : `bg-black`}`}></div>
+				<div className={`h-[4px] w-8 translate-y-2  ease-out duration-200 ${visibility ? `translate-x-full opacity-0` : `translate-x-0 bg-black`}`}></div>
+				<div className={`h-[4px] w-8 translate-y-4 ease-out duration-200 origin-top-right ${visibility ? `rotate-45 bg-white` : `bg-black`}`}></div>
+			</div>
+			<div className={`z-10 ${visibility ? `text-white` : `text-black`} md:text-black`}>Suman.portflio</div>
+			<ul className={`absolute md:relative bg-blue-700/90 md:bg-white flex flex-col md:flex-row h-screen md:h-auto w-screen md:w-auto items-center justify-center text-4xl md:text-base text-center md:text-left gap-3  ${visibility ? `translate-x-0 text-white` : `-translate-x-full`} md:text-black md:translate-x-0 transition-transform transit ease-in-out`}>
+				<li><a href="#home" onClick={()=> setVisibility(false)}>Home</a></li>
+				<li><a href="#about" onClick={()=> setVisibility(false)}>About</a></li>
+				<li><a href="#projects" onClick={()=> setVisibility(false)}>Projects</a></li>
+				<li><a href="#contact" onClick={()=> setVisibility(false)}>Contact</a></li>
+			</ul>
 		</nav>
 	)
 }
